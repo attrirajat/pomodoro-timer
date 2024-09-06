50f850a7322538e3c7e96560c9a2fdac23d7f225
Rajat Attri <rajatattri@Rajats-MacBook-Air.local>
2024-09-06 22:42:44 +0100

    updated code to run breaks

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 let timerInterval;
 let minutes = 25;
-let seconds = 60;
+let seconds = 0;
 let isRunning = false;
 
 const startPauseButton = document.getElementById("startPauseButton");
+
+function togglePomodoro() {
+  if (isRunning) {
+    clearInterval(timerInterval);
+    startPauseButton.innerHTML = "Start";
+  } else {
+    timerInterval = setInterval(() => {
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(timerInterval);
+          alert("Pomodoro session completed! Take a break!");
+          return;
+        } else {
+          minutes -= 1;
+          seconds = 59;
+        }
+      } else {
+        seconds -= 1;
+      }
+      updateDisplay();
+    }, 1000);
+
+    startPauseButton.innerHTML = "Pause";
+  }
+
+  isRunning = !isRunning;
+}
 
 function updateDisplay() {
   document.getElementById("seconds").innerHTML = String(seconds).padStart(
@@ -16,24 +43,34 @@ function updateDisplay() {
   );
 }
 
-function togglePomodoro() {
-  if (isRunning) {
-    clearInterval(timerInterval);
-    startPauseButton.innerHTML = "Start";
-  } else {
-    timerInterval = setInterval(() => {
-      seconds -= 1;
-      if (minutes == 25) {
-        minutes -= 1;
-      }
-
-      if (seconds === 0) {
-        seconds = 60;
-        minutes -= 1;
-      }
-      updateDisplay();
-    }, 1000);
-    startPauseButton.innerHTML = "Pause";
-  }
-  isRunning = !isRunning;
+function pomodoroMode() {
+  resetPomodoro();
+  document.body.style.background = "#9f4e4e";
+  minutes = 25;
+  updateDisplay();
 }
+
+function shortbreakMode() {
+  resetPomodoro();
+  document.body.style.background = "#418348";
+  minutes = 5;
+  updateDisplay();
+}
+
+function longbreakMode() {
+  resetPomodoro();
+  document.body.style.background = "#355c7d";
+  minutes = 15;
+  updateDisplay();
+}
+
+function resetPomodoro() {
+  clearInterval(timerInterval); // Stop any running interval
+  minutes = 25;
+  seconds = 0;
+  isRunning = false;
+  document.getElementById("startPauseButton").innerHTML = "Start";
+  updateDisplay();
+}
+
+updateDisplay();
